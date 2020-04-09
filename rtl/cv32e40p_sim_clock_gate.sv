96f79837f28ddab404e5a7e0a7af765a457aa663
Piotr Binkowski <pbinkowski@antmicro.com>
2020-04-09 14:32:48 +0200

    disable clock gating

--- rtl/cv32e40p_sim_clock_gate.sv
+++ rtl/cv32e40p_sim_clock_gate.sv
@@ -8,6 +8,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+`define PULP_FPGA_EMUL
+
 module cv32e40p_clock_gate
 (
     input  logic clk_i,
