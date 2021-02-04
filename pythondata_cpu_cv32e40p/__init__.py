67acce48ec7461943ffc92b8cd8e2a40ce811afb
LiteX Robot <robot@timvideos.us>
2021-02-04 04:21:24 +0000

    Updating pythondata-cpu-cv32e40p to 0.0.post94
    Updated data to v0.0-10-g087cb61 based on 087cb61a182f6bc4f757d3430865482aaff61cba from https://github.com/antmicro/cv32e40p.
    > commit 087cb61a182f6bc4f757d3430865482aaff61cba
    > Author: Piotr Binkowski <pbinkowski@antmicro.com>
    > Date:   Wed May 20 14:40:57 2020 +0200
    >
    >     disable sim tracer
    >
    
    Updated using 0.0.post84 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40p/__init__.py
+++ pythondata_cpu_cv32e40p/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/antmicro/cv32e40p"
 
 # Module version
-version_str = "0.0.post92"
-version_tuple = (0, 0, 92)
+version_str = "0.0.post94"
+version_tuple = (0, 0, 94)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post92")
+    pversion = V("0.0.post94")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Wed May 20 14:40:57 2020 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post82"
-tool_version_tuple = (0, 0, 82)
+tool_version_str = "0.0.post84"
+tool_version_tuple = (0, 0, 84)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post82")
+    ptool_version = V("0.0.post84")
 except ImportError:
     pass
 
