70104e711fbfc6439d6518ce9860609e5ac5594c
Jiaxun Yang <jiaxun.yang@flygoat.com>
2024-12-28 15:44:53 +0000

    Updating pythondata-cpu-cv32e40p to 1.0.1.post1909
    Updated data to v1.0.1-1764-g360d272 based on 360d272898d81806be3377193870dbf83a3ea79f from https://github.com/openhwgroup/cv32e40p.
    > commit 360d272898d81806be3377193870dbf83a3ea79f
    > Merge: 782283a 1079a83
    > Author: Davide Schiavone <davide@openhwgroup.org>
    > Date:   Sat Jul 13 09:43:40 2024 +0200
    >
    >     Merge pull request #1026 from openhwgroup/dev
    >
    >     merge dev into master
    >
    
    Updated using 0.0.post145 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40p/__init__.py
+++ pythondata_cpu_cv32e40p/__init__.py
@@ -1,42 +1,45 @@
 import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "system_verilog")
-src = "https://github.com/antmicro/cv32e40p"
+src = "https://github.com/openhwgroup/cv32e40p"
 
 # Module version
-version_str = "0.0.post152"
-version_tuple = (0, 0, 152)
+version_str = "1.0.1.post1909"
+version_tuple = (1, 0, 1, 1909)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post152")
+    pversion = V("1.0.1.post1909")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10"
-data_version_tuple = (0, 0, 10)
+data_version_str = "1.0.1.post1764"
+data_version_tuple = (1, 0, 1, 1764)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10")
+    pdata_version = V("1.0.1.post1764")
 except ImportError:
     pass
-data_git_hash = "087cb61a182f6bc4f757d3430865482aaff61cba"
-data_git_describe = "v0.0-10-g087cb61"
+data_git_hash = "360d272898d81806be3377193870dbf83a3ea79f"
+data_git_describe = "v1.0.1-1764-g360d272"
 data_git_msg = """\
-commit 087cb61a182f6bc4f757d3430865482aaff61cba
-Author: Piotr Binkowski <pbinkowski@antmicro.com>
-Date:   Wed May 20 14:40:57 2020 +0200
+commit 360d272898d81806be3377193870dbf83a3ea79f
+Merge: 782283a 1079a83
+Author: Davide Schiavone <davide@openhwgroup.org>
+Date:   Sat Jul 13 09:43:40 2024 +0200
 
-    disable sim tracer
+    Merge pull request #1026 from openhwgroup/dev
+    
+    merge dev into master
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post142"
-tool_version_tuple = (0, 0, 142)
+tool_version_str = "0.0.post145"
+tool_version_tuple = (0, 0, 145)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post142")
+    ptool_version = V("0.0.post145")
 except ImportError:
     pass
 
